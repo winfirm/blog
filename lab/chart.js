70d0196127eee263294db8dd4dd5ea98be4171f1
Winfirm <33377544+winfirm@users.noreply.github.com>
2023-02-22 20:35:38 +0800

    Update chart.js

--- lab/chart.js
+++ lab/chart.js
@@ -337,6 +337,7 @@ function reload_symbols(){
 	type:'get',
 	url:url,
 	data:'',
+	headers: {  'Access-Control-Allow-Origin': 'https://winfirm.net' },
 	success: function(result ) {
 		  let obj = JSON.parse(result);		  
 		  let array= obj.forex;
